566a2a86757920b22ca2248db606a0de86fa9958
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-14 17:44:17 +0300

    Configure ap, initialize extensions and register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,25 @@
+from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from .config import config_options
+
+bootstrap = Bootstrap()
+
+def Create_app(config_name):
+    app = Flask(__name__)
+
+     # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    #Regstering blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    return app
+
