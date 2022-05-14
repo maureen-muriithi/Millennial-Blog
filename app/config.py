752f85637a19bbcfb0dd57c86c9d4f96102cb074
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-14 17:51:36 +0300

    Add configuration classes

---
+++ app/config.py
@@ -0,0 +1,35 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moh:mjwm2222@localhost/blogs'
+    SECRET_KEY = 'secretkey'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development': DevConfig,
+'production': ProdConfig
+}
