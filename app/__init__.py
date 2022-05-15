cba63a1d0b5a80835fdb1090114add2b6221b575
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-16 01:16:02 +0300

    Enable uploading and storage of photos

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from .config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -11,6 +12,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos', IMAGES)
 
 def create_app(config_name):
 
@@ -18,6 +20,9 @@ def create_app(config_name):
 
      # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+     # configure UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
