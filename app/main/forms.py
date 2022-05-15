eebab4156404f1ae3cddbe1d61439eaa34e5b642
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-16 01:04:38 +0300

    Factor logic for the profile and update profile

---
+++ app/main/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email
+from flask_login import current_user
+from wtforms import StringField, TextAreaField, SubmitField, ValidationError
+from flask_wtf.file import FileField, FileAllowed
+from ..models import User
+
+class UpdateProfile(FlaskForm):
+    username = StringField('Enter Your Username', validators=[DataRequired()])
+    bio = TextAreaField('Write a brief bio about you.',validators = [DataRequired()])
+    profile_picture = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username = username.data).first():
+                raise ValidationError("The username has already been taken")
+
+class AddBlog(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    content = TextAreaField('Blog Content',validators=[DataRequired()])
+    submit = SubmitField('Post')
--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,47 @@
 from . import main
-from flask import render_template,request, redirect, url_for
+from flask import render_template,request, redirect, url_for, abort
 from flask_login import login_required
+from ..models import User, Blog
+from .forms import UpdateProfile, AddBlog
+from .. import db
 
 @main.route('/')
 def index():
 
     title = 'Welcome to Millennial Blogs'
-    message = 'I love Python'
 
     return render_template('index.html', title=title, )
 
-@main.route('/profile')
+@main.route('/user/<username>')
 @login_required
-def profile():
+def profile(username):
 
     title = 'your profile page'
+    user = User.query.filter_by(username = username).first()
 
-    return render_template('profile/profile.html', title=title)
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", title = title, user = user)
+
+@main.route('/user/<username>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(username):
+    user = User.query.filter_by(username = username).first()
+    if user is None:
+        abort(404)
+
+    update_form = UpdateProfile()
+
+    if update_form.validate_on_submit():
+        user.bio = update_form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',username=user.username))
+
+    return render_template('profile/update.html',update_form =update_form)
 
 @main.route('/blogs')
 def blogs():
