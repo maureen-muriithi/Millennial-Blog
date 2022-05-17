3f95415d2b744f5dc02a941b4f0b3896251c1853
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-17 03:44:30 +0300

    Update tests

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,6 +1,11 @@
-from app.models import Blog, User
+from app.models import Blog, User, Comment
 from app import db
 def setUp(self):
-        self.user_Moh = User(username = 'James',password = 'potato', email = 'james@ms.com')
-        # self.new_blog = Blog(movie_id=12345,movie_title='Review for movies',image_path="https://image.tmdb.org/t/p/w500/jdjdjdjn",movie_review='This movie is the best thing since sliced bread',user = self.user_James )
+        self.user_Moh = User(username = 'Moh',password = 'potato', email = 'moh@ms.com')
+        self.new_blog = Blog(id=1, title='A blog about busness', post='ctfgvbhjnmk l ,mkj nhbgv jcfhd restfyk ulhnmkoj nhbgv jfchdxsg  edrftgyhu ljmkn bmvncxd hcfjgykuhjnk. bvngcfxd' )
+        self.new_comment = Comment(comment = "Nice", post_id = self.new_blog.id, user_id = self.user_Moh.id)
 
+def test_instance(self):
+        self.assertTrue(isinstance(self.user_Moh, User))
+        self.assertTrue(isinstance(self.new_blog, Blog))
+        self.assertTrue(isinstance(self.new_comment, Comment))
