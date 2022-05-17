85e8ace940560154858aff35985cbaa1d6dbc0cf
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-17 04:04:49 +0300

    Attempt deployment

--- app/config.py
+++ app/config.py
@@ -25,7 +25,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moh:mjwm2222@localhost/blogs_test'
